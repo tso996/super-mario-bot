87825fc065780ee61480b153ae1eb1412638943f
Joe <51638822+tso996@users.noreply.github.com>
2022-04-25 20:30:04 +0100

    set up the render code

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 # bot
-import gym_super_mario_bros
+# load up the dependencies
+import gym_super_mario_bros as gsmb
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+
+# setup the game
+env = gsmb.make('SuperMarioBros-v0')
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+print(SIMPLE_MOVEMENT)
+
+done = True
+for step in range(100000):
+    if done:
+        env.reset()
+    state, reward, done, info = env.step(env.action_space.sample())
+    env.render()
+env.close
 
 print("done!")
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 -This project uses the OpenAI Gym. https://gym.openai.com/
+
 -It also uses the nes-py to use the controller to play the game.
+
 -Install the dependencies from the requirements.txt.
 
 ```pip3 install -r requirements.txt```
